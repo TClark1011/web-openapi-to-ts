5d9e43ca9bc6b3c5ceb8dae846da6a0ca92bc146
Thomas Clark <thomassiclark@gmail.com>
2025-04-30 17:37:44 +1000

    finished

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,26 +2,34 @@ import { fail } from "@sveltejs/kit";
 import type { Actions } from "./$types";
 import openapiTS, { astToString } from "openapi-typescript";
 import { safeJSONParse } from "$lib/utils";
+import { m } from "$lib/paraglide/messages";
 
 export const actions = {
 	default: async ({ request }) => {
 		const formData = await request.formData();
-		const spec = formData.get("spec");
+		const spec = formData.get("spec")?.toString()?.trim();
 
 		if (!spec) {
-			return fail(400, { spec: "Spec is required" });
+			return fail(400, { success: false, message: m["errors.no_spec"]() });
 		}
 
 		const parsed = safeJSONParse(spec.toString());
 
-		if (!parsed) {
-			return fail(400, { spec: "Spec is not valid JSON" });
+		if (parsed === undefined) {
+			return fail(400, { success: false, message: m["errors.invalid_json"](), });
 		}
 
-		const typescriptAst = await openapiTS(parsed);
+		const typescriptAst = await openapiTS(parsed).catch(() => undefined);
+
+		if (!typescriptAst) {
+			return fail(400, { success: false, message: m["errors.invalid_openapi"]() });
+		}
 
 		const typescriptCode = astToString(typescriptAst);
 
-		return typescriptCode.trim();
+		return {
+			success: true,
+			typescriptCode,
+		}
 	}
 } satisfies Actions;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,34 @@
 	import type { PageProps } from './$types';
 
 	let { form }: PageProps = $props();
+
+	let resultElement: HTMLTextAreaElement | undefined = $state();
+	let copyButtonLabel = $state(m.copy());
+
+	const copyResultToClipboard = async () => {
+		if (!resultElement) return;
+
+		resultElement.select();
+		resultElement.setSelectionRange(0, 99999); // For mobile devices
+		await navigator.clipboard.writeText(resultElement.value);
+
+		copyButtonLabel = m.copy_success();
+		setTimeout(() => {
+			copyButtonLabel = m.copy();
+		}, 2000);
+	};
+
+	$effect(() => {
+		if (!form || form.success) return; // no submission yet
+
+		window.alert(form.message);
+	});
 </script>
+
+<div class="prose p-4">
+	<h1>{m.page_title()}</h1>
+	<p>{m.page_description()}</p>
+</div>
 
 <div class="flex w-screen flex-wrap justify-center gap-4 p-4">
 	<form
@@ -20,14 +47,11 @@
 	>
 		<fieldset class="fieldset">
 			<legend class="fieldset-legend">{m.open_api_spec()}</legend>
-			<textarea
-				rows="20"
-				class="textarea border-base-content w-full resize-none rounded-md border-2 font-mono"
-				name="spec"
+			<textarea rows="20" class="textarea w-full resize-none rounded-md font-mono" name="spec"
 			></textarea>
 		</fieldset>
 
-		<button class="btn" type="submit">
+		<button class="btn btn-primary" type="submit">
 			{m.generate_ts()}
 		</button>
 	</form>
@@ -35,11 +59,15 @@
 		<fieldset class="fieldset">
 			<legend class="fieldset-legend">{m.generated_code()}</legend>
 			<textarea
+				bind:this={resultElement}
 				rows="20"
-				value={form?.toString?.() ?? ''}
+				value={form?.typescriptCode ?? ''}
 				readonly
 				class="textarea w-full resize-none font-mono"
 			></textarea>
 		</fieldset>
+		<button class="btn btn-secondary w-full" onclick={copyResultToClipboard}>
+			{copyButtonLabel}
+		</button>
 	</div>
 </div>
