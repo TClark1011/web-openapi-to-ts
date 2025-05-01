dcb211314e0b5032fed92f069cd2937a8267cbad
Thomas Clark <thomassiclark@gmail.com>
2025-05-01 16:05:38 +1000

    metadata

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,9 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<title>OpenAPI to Typescript Code</title>
+	<meta name="description" content="Convert OpenAPI specs to TypeScript code from your browser" />
+</svelte:head>
+
 {@render children()}
